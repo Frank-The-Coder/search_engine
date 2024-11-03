45b7ec0fb5c252349e5baf197b6521ed86e4a84e
andrewcleung <leung.andrew@hotmail.com>
2024-11-03 12:46:24 -0500

    no deploy redirect setup

--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 import os
 
 IS_DEPLOY = os.getenv("DEPLOY_MODE") == "true"
-MAIN_URL = "http://ec2-23-22-39-109.compute-1.amazonaws.com" if IS_DEPLOY else "http://localhost:8080"
+MAIN_URL = "http://localhost:8080"
